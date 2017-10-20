303652866bd895a07b994c4b61184f9984ab416b
Jorge M <jorgem@gmail.com>
2017-10-20 18:49:11 -0500

    CFD 3.3 inicial

--- CFDiTOexcel.php
+++ CFDiTOexcel.php
@@ -1,38 +1,35 @@
 <?php
    /*
-   * lee información de CFDi y genera salida en csv delimitado por comas
+   * Lee información de CFDi y genera salida en csv delimitado por comas
    * (configurable), útil para generar reportes para excel.
    * @ToRo 2016-11-15 https://tar.mx/tema/facturacion-electronica.html
+   * soporta CFDi 3.2 y 3.3.
    */
    // Configuración
+   require_once("catalogos.php");//catálogo de conceptos CFD 3.2/3.3
    if(isset($argv[1]) && is_dir($argv[1])) $path = $argv[1];
-   else $path="directorio/facturas"; // el directorio donde se tienen los archivos XML
+   else $path="facturas/";       // el directorio donde se tienen los archivos XML
    if(!is_dir($path)) die("Estableza la ruta de sus XML o ejecute php ".$argv[0]." DIRECTORIO\n".$path." no existe\n\n");
-   $archivo= "resumen.csv";      //nombre del archivo a generar
+   $archivo= "resumen.csv";      // nombre del archivo a generar
    //
-   $saltolinea = "\r\n";         //para windows usar \r\n, para mac o linux \n
-   $separador  = ",";            //separador de campos
-   if(!is_dir($path)) die("\nError: No existe el directorio ".$path.$saltolinea);
-   //
-   //
-   // lee 3 niveles de profundidad
-   // d1/d2/d3
-   //
-   $pathd=dir($path);
-   $t=0;
+   $salto = "\r\n";              // para windows usar \r\n, para mac o linux \n
+   $sep  = ", ";                 // separador de campos
+   if(!is_dir($path)) die("\nError: No existe el directorio ".$path.$salto);
+   // 3 niveles de profundidad   // d1/d2/d3
+   $pathd=dir($path); $t=0;
    while(false !== ($e0 = $pathd->read())) {
       if(preg_match("/^\./",$e0)) continue;
-      elseif(preg_match("/\.xml/i",$e0)) { $archivos["/"][] = $e0;$t++; }
+      elseif(preg_match("/\.xml$/i",$e0)) { $archivos["/"][] = $e0;$t++; }
       elseif(is_dir($path."/".$e0)) {
          $pathd1 = dir($path."/".$e0);
          while(false !== ($e1 = $pathd1->read())) {
             if(preg_match("/^\./",$e1)) continue;
-            elseif(preg_match("/\.xml/i",$e1)) { $archivos["/".$e0."/"][] = $e1;$t++; }
+            elseif(preg_match("/\.xml$/i",$e1)) { $archivos["/".$e0."/"][] = $e1;$t++; }
             elseif(is_dir($path."/".$e0."/".$e1)) {
                $pathd2 = dir($path."/".$e0."/".$e1);
                while(false !== ($e2 = $pathd2->read())) {
                   if(preg_match("/^\./",$e2)) continue;
-                  elseif(preg_match("/\.xml/i",$e2)) { $archivos["/".$e0."/".$e1."/"][] = $e2; $t++; }
+                  elseif(preg_match("/\.xml$/i",$e2)) { $archivos["/".$e0."/".$e1."/"][] = $e2; $t++; }
                }
                $pathd2->close();
             }
@@ -43,102 +40,128 @@
    $pathd->close();
    //
    $no=strlen($t)+1;
-   //encabezados
-   $resumend  = "FACTURA".$separador."FOLIO FISCAL".$separador."SUBTOTAL".$separador."TOTAL".$separador."FECHA";
-   $resumend .= $separador."MÉTODO PAGO";
-   $resumend .= $separador."EMITE RFC".$separador."EMITE RAZÓN".$separador."RECEPTOR RFC".$separador."RECEPTOR RAZÓN";
-   $resumend .= $separador."IMPUESTOS TRASLADADOS".$separador."IMPUESTOS RETENIDOS".$separador."CONCEPTOS";
-   $resumend .= $saltolinea;
+   //encabezado csv
+   $salida = null;
+   foreach($csv AS $k=>$v) { $salida .= $k.$sep; }
+   $salida .= $salto;
+   //
    if(!empty($archivos)) {
       $i=0;
       foreach($archivos AS $dir=>$files) {
          foreach($files AS $file) {
             $xmlfile = $path.$dir.$file;
             if(is_file($xmlfile)) {
-               $xmldata = parseCFDi($xmlfile);
-               if(!isset($xmldata['id'])) continue;
+               $xml= parse($xmlfile);
+               if(!isset($xml->id)) { printf("El archivo %s no parece ser un CFDi válido\n",$xmlfile); continue; }
                $i++;
                printf("%".$no."d",$i);
                echo " ".$dir.$file." ";
-               $resumend .= $xmldata['serie'].$xmldata['folio'].$separador.$xmldata['id'].$separador;
-               $resumend .= $xmldata['subtotal'].$separador.$xmldata['total'].$separador;
-               $resumend .= substr($xmldata['fecha'],0,10);
-               $resumend .= $separador.str_replace(","," ",$xmldata['metodopago']);
-               $resumend .= $separador.$xmldata['emisor'][0].$separador.str_replace(","," ",$xmldata['emisor'][1]);
-               $resumend .= $separador.$xmldata['receptor'][0].$separador.str_replace(","," ",$xmldata['receptor'][1]);
-               $resumend .= $separador.$xmldata['impuestos'][0].$separador.($xmldata['impuestos'][1]+0).$separador;
-               if(!empty($xmldata['concepto'])) {
-                  foreach($xmldata['concepto'] AS $con) {
-                     $resumend .= str_replace(","," ",$con[4])." - ";
+               foreach($csv AS $a=>$b) {
+                  if(is_array($b)) {
+                     foreach($b AS $z) { $salida .= $xml->{$z}; }
+                  } elseif(preg_match("/\//",$b)) {
+                     $tmp = explode("/",$b);
+                     $salida .= str_replace(","," ",$xml->{$tmp[0]}->{$tmp[1]});
+                  } else {
+                     $salida .= str_replace(","," ",$xml->{$b});
+                  }
+                  $salida .= $sep;
+               }
+               foreach($csvimpuestos AS $a=>$b) {
+                  if(isset($xml->{$a})) {
+                     foreach($xml->{$a} AS $x=>$y) {
+                     }
                   }
                }
-               $resumend .= $saltolinea;
-               //print_r($xmldata);
+               die($salida.$salto);
+               //$salida .= $xml->serie.$xml->folio.$sep.$xml->id.$sep.$xml->subtotal.$sep.$xml->total.$sep;
+               $salida .= $salto;
                echo " ok!\n";
             }
          }
       }
    }
-   if(file_put_contents($archivo,$resumend)) {
-      echo $saltolinea.$saltolinea."Se almacenó el archivo $archivo".$saltolinea.$saltolinea;
+   echo $salida;
+   //print_r($xml);
+   /* activar 
+   if(file_put_contents($archivo,$salida)) {
+      echo $salto.$salto."Se almacenó el archivo $archivo".$salto.$salto;
    } else {
-      echo $saltolinea."NO SE PUDO ALMACENAR EL ARCHIVO $archivo, verifique que pueda escribir ahí.".$saltolinea;
+      echo $salto."NO SE PUDO ALMACENAR EL ARCHIVO $archivo, verifique que pueda escribir ahí.".$salto;
    }
+   */
    /*
    * lee el cfdi y regresa un arreglo de datos.
+   * v3.2 y v3.3
    */
-   function parseCFDi() {
-      @$tmp = func_get_arg(0);
-      @$xml = simplexml_load_file($tmp);
+   function parse($file) {
+      global $cabeza,$emisor,$receptor,$impuestos,$conceptos;
+      $datat = file_get_contents($file);
+      @$xml= simplexml_load_file($file);
       @$ns = $xml->getNamespaces(true);
       @$xml->registerXPathNamespace('c', $ns['cfdi']);
-      @$xml->registerXPathNamespace('t', $ns['tfd']);
-      if(!isset($ns['cfdi'])) {
-         echo "=== no pude leer $tmp como CFDi :(\n";
-         return;
+      $xml->registerXPathNamespace('t', $ns['tfd']);
+      $cfdi = $xml->xpath('//cfdi:Comprobante');
+      $datos = new stdclass;
+      $datos->id = null;
+      //
+      $xml2=new DOMDocument();
+      $ok = $xml2->loadXML($datat);
+      $root = $xml2->getElementsByTagName('Comprobante')->item(0);
+      libxml_use_internal_errors(true);   // Gracias a Salim Giacoman
+      $xml2 = new DOMDocument();
+      $datos->version = $ver = ($root->getAttribute('Version') != null) ? $root->getAttribute('Version') : $root->getAttribute('version');
+      // cabeceras
+   foreach($cabeza["3.3"] AS $tag=>$x) {
+      $datos->{$tag} = (string) $root->getAttribute($cabeza[$ver][$tag]);
       }
-      $cfdiComprobante = $xml->xpath('//cfdi:Comprobante');
-      $datos = Array('id'=>'');
-      foreach ($xml->xpath('//cfdi:Comprobante') as $cfdiComprobante) {
-         $fecha = strtotime($cfdiComprobante['fecha']);
-         $datos['fecha'] = date('Y-m-d H:i:s',$fecha);
-         $datos['total']= (string)$cfdiComprobante['total'];
-         $datos['subtotal']=(string)$cfdiComprobante['subTotal'];
-         $datos['descuento']=(string)$cfdiComprobante['descuento'];
-         $datos['formapago']=(string)$cfdiComprobante['formaDePago'];
-         $datos['tipo']=(string)$cfdiComprobante['tipoDeComprobante'];
-         $datos['expedido']=(string)$cfdiComprobante['LugarExpedicion'];
-         $datos['metodopago']=(string)$cfdiComprobante['metodoDePago'];
-         $datos['nocertificado']=(string)$cfdiComprobante['noCertificado'];
-         $datos['serie']=(string)$cfdiComprobante['serie'];
-         $datos['folio']=(string)$cfdiComprobante['folio'];
-         foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Emisor') as $Emisor) {
-            $datos['emisor'] = array((string)$Emisor['rfc'],(string)$Emisor['nombre']);
-         }
-         foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Receptor') as $Receptor){
-            $datos['receptor']=array((string)$Receptor['rfc'],(string)$Receptor['nombre']);
-         }
-         foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Impuestos') as $Receptor){
-            $datos['impuestos']=array((string)$Receptor['totalImpuestosTrasladados'],(string)$Receptor['totalImpuestosRetenidos']);
-         }
-         $i=0;
-         foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Conceptos//cfdi:Concepto') as $Concepto) {
-            $i++;
-            $datos['concepto'][$i] = array((string)$Concepto['unidad'], (string)$Concepto['importe'], (string)$Concepto['cantidad'], (string)$Concepto['valorUnitario'], (string)$Concepto['descripcion']);
-         }
-         foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Conceptos//cfdi:Concepto') as $Concepto) {
-            $i++;
-            @$datos['traslado'][$tmp] = array((string)$Traslado['tasa'],(string)$Traslado['importe']);
-         }
-         foreach ($xml->xpath('//cfdi:Comprobante//cfdi:Impuestos//cfdi:Retenciones//cfdi:Retencion') as $Traslado) {
-            $tmp = (string) $Traslado['impuesto'];
-            $datos['retencion'][$tmp] = (string)$Traslado['importe'];
-         }
-         foreach ($xml->xpath('//t:TimbreFiscalDigital') as $tfd) {
-            $datos['id'] = (string)$tfd['UUID'];
-            $datos['id'] = strtoupper($datos['id']);
-            $datos['timbrado'] = date('Y-m-d H:i:s',strtotime($tfd['FechaTimbrado']));
+      // emisor
+      $emite = $root->getElementsByTagName('Emisor')->item(0);
+      $datos->emisor = new stdclass;
+      foreach($emisor["3.3"] AS $tag=>$x) {
+         $datos->emisor->{$tag} = (string) $emite->getAttribute($emisor[$ver][$tag]);
+      }
+      // emisor.regimen
+      if($ver=="3.2") {
+         $tmp = $root->getElementsByTagName('RegimenFiscal')->item(0);
+         $datos->emisor->regimen = (string)$tmp->getAttribute('Regimen');
+      }
+      // receptor
+      $recibe= $root->getElementsByTagName('Receptor')->item(0);
+      $datos->receptor = new stdclass;
+      foreach($receptor["3.3"] AS $tag=>$x) {
+         $datos->receptor->{$tag} = (string) $recibe->getAttribute($receptor[$ver][$tag]);
+      }
+      // impuestos
+      $impuesto = new stdclass;
+      $impuesto->trasladados = $impuesto->retenidos = [];
+      $imp= $root->getElementsByTagName('Impuestos');
+      foreach(['Traslado' => 'trasladados','Retencion' => 'retenidos'] AS $x=>$y) {
+         $traslado = $imp->item(0)->getElementsByTagName($x); $i = 0;
+         foreach($traslado as $t) {
+            $i++; $impuesto->{$y}[$i] = new stdclass;
+            foreach($impuestos["3.3"] AS $a=>$b) {
+               if(isset($impuestos[$ver][$a])) $impuesto->{$y}[$i]->{$a} = $t->getAttribute($impuestos[$ver][$a]);
+            }
          }
       }
+      $datos->impuestos = $impuesto;
+      // conceptos
+      $losconceptos=[];
+      $concepto= $root->getElementsByTagName('Concepto');
+      $i=0;
+      foreach($concepto AS $t) {
+         $i++; $losconceptos[$i] = new stdclass;
+         foreach($conceptos["3.3"] AS $a=>$b) {
+            if(isset($conceptos[$ver][$a])) $losconceptos[$i]->{$a} = $t->getAttribute($conceptos[$ver][$a]);
+         }
+      }
+      $datos->conceptos = $losconceptos;
+      $tfd = $root->getElementsByTagName('TimbreFiscalDigital')->item(0);
+      $datos->id = $tfd->getAttribute('UUID');
+      $datos->fechatimbrado = date('Y-m-d H:i:s',strtotime($tfd->getAttribute('FechaTimbrado')));
+      $datos->fecha = date('Y-m-d H:i:s',strtotime($datos->fecha));
+      //
+      unset($tfd,$root,$imp,$tmp);
       return $datos;
    }
--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # UtilsCFDi
 
-utilidades para leer o manejar archivos CFDi (Factura Electrónica en México).
+Utilidades para leer o manejar archivos CFDi (Factura Electrónica en México).
 
 
 https://tar.mx/tema/facturacion-electronica.html
+
+[Genera CSV de un directorio de facturas](CFDiTOexcel.php) versiones 3.2 y 3.3
